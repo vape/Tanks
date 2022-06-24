5239a519287ddc372d5b452c8bc58fd7ef0c5712
Artem Sokolov <sokolov.artem@hotmail.com>
2022-06-24 21:38:51 +0300

    Add simple player camera follower.

---
+++ Assets/Source/Game/PlayerCameraFollower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Tanks.Game
+{
+    [RequireComponent(typeof(Camera))]
+    public class PlayerCameraFollower : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject player;
+        [SerializeField]
+        private Vector3 offset;
+        [SerializeField]
+        private float smoothness;
+
+        private Vector3 velocity;
+
+        private void LateUpdate()
+        {
+            if (player != null)
+            {
+                UpdatePositionAndRotation();
+            }
+        }
+
+        private void UpdatePositionAndRotation()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, smoothness);
+            transform.LookAt(player.transform.position);
+        }
+    }
+}
