bbaa6ba68b99b4b89e91fd0815fd66308fb18ef1
Artem Sokolov <sokolov.artem@hotmail.com>
2022-06-24 23:09:46 +0300

    Add simple test 'enemy' that follows player.

---
+++ Assets/Source/Game/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Tanks.Game
+{
+    public class EnemyController : MonoBehaviour
+    {
+        public const string DamageAreaTag = "DamageArea";
+
+        [SerializeField]
+        private NavMeshAgent navMeshAgent;
+
+        private void Update()
+        {
+            if (WorldEntitiesManager.Instance.Player != null)
+            {
+                navMeshAgent.destination = WorldEntitiesManager.Instance.Player.transform.position;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == DamageAreaTag)
+            {
+                navMeshAgent.stoppingDistance = navMeshAgent.remainingDistance;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == DamageAreaTag)
+            {
+                navMeshAgent.stoppingDistance = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Game/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Tanks.Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            WorldEntitiesManager.Instance.RegisterPlayer(this);
+        }
+
+        private void OnDisable()
+        {
+            WorldEntitiesManager.Instance.UnregisterPlayer(this);
+        }
+    }
+}
